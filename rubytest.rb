1f02f1b39350d3cac96d00c63d29f1cd55b0cc48
Michael <malarconii@yahoo.com>
2019-01-11 09:48:31 -0800

    completed a to z test

--- rubytest.rb
+++ rubytest.rb
@@ -1,31 +1,33 @@
-# Write a method that takes a string and returns true if it is a
-# palindrome. A palindrome is a string that is the same whether written
-# backward or forward. Assume that there are no spaces; only lowercase
-# letters will be given.
+# Write a method that takes a string in and returns true if the letter
+# "z" appears within three letters **after** an "a". You may assume
+# that the string contains only lowercase letters.
 #
-# Difficulty: easy.
+# Difficulty: medium.
+# loop through the string until you find an a
+# when you hit a - check the index of a +1, +2, +3 for z
 
-# loop through the string
-# compare end character to starting character at each iteration
-# break loop if they are ever not equal and return false, else return true
-
-def palindrome?(string)
+def nearby_az(string)
   i = 0
   while i < string.length
-    if string[i] != string[(string.length - 1) - i]
-      return false 
+    if string[i] == "a"
+      if string[i + 1] == "z" || string[i + 2] == "z" || string[i + 3] == "z"
+        return true
+      end
     end
     i += 1
   end
-  return true
+  return false
 end
 
 # These are tests to check that your code is working. After writing
 # your solution, they should all print true.
 
-puts("\nTests for #palindrome?")
+puts("\nTests for #nearby_az")
 puts("===============================================")
-    puts('palindrome?("abc") == false: ' + (palindrome?('abc') == false).to_s)
-    puts('palindrome?("abcba") == true: ' + (palindrome?('abcba') == true).to_s)
-    puts('palindrome?("z") == true: ' + (palindrome?('z') == true).to_s)
+    puts('nearby_az("baz") == true: ' + (nearby_az('baz') == true).to_s)
+    puts('nearby_az("abz") == true: ' + (nearby_az('abz') == true).to_s)
+    puts('nearby_az("abcz") == true: ' + (nearby_az('abcz') == true).to_s)
+    puts('nearby_az("a") == false: ' + (nearby_az('a') == false).to_s)
+    puts('nearby_az("z") == false: ' + (nearby_az('z') == false).to_s)
+    puts('nearby_az("za") == false: ' + (nearby_az('za') == false).to_s)
 puts("===============================================")
