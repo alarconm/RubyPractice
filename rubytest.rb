9b5407b9a35b3bf9982f91a6c505d4d752f12625
Michael <malarconii@yahoo.com>
2019-01-11 09:40:26 -0800

    completed palindrome problem

--- rubytest.rb
+++ rubytest.rb
@@ -1,35 +1,31 @@
-# Write a method that takes a string and returns the number of vowels
-# in the string. You may assume that all the letters are lower cased.
-# You can treat "y" as a consonant.
+# Write a method that takes a string and returns true if it is a
+# palindrome. A palindrome is a string that is the same whether written
+# backward or forward. Assume that there are no spaces; only lowercase
+# letters will be given.
 #
 # Difficulty: easy.
 
-# define list of vowels - let's use an array
-# loop through each character of the string - add to counter for each
-# vowel that is found
+# loop through the string
+# compare end character to starting character at each iteration
+# break loop if they are ever not equal and return false, else return true
 
-def count_vowels(string)
-  vowel_count = 0
-  vowels = ["a", "e", "i","o","u"]
+def palindrome?(string)
   i = 0
-
   while i < string.length
-    if vowels.include?(string[i]) == true
-      vowel_count += 1
+    if string[i] != string[(string.length - 1) - i]
+      return false 
     end
     i += 1
   end
-  
-  return vowel_count
+  return true
 end
 
 # These are tests to check that your code is working. After writing
 # your solution, they should all print true.
 
-puts("\nTests for #count_vowels")
+puts("\nTests for #palindrome?")
 puts("===============================================")
-    puts('count_vowels("abcd") == 1: ' + (count_vowels('abcd') == 1).to_s)
-    puts('count_vowels("color") == 2: ' + (count_vowels('color') == 2).to_s)
-    puts('count_vowels("colour") == 3: ' + (count_vowels('colour') == 3).to_s)
-    puts('count_vowels("cecilia") == 4: ' + (count_vowels('cecilia') == 4).to_s)
+    puts('palindrome?("abc") == false: ' + (palindrome?('abc') == false).to_s)
+    puts('palindrome?("abcba") == true: ' + (palindrome?('abcba') == true).to_s)
+    puts('palindrome?("z") == true: ' + (palindrome?('z') == true).to_s)
 puts("===============================================")
